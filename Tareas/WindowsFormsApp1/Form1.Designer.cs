a0e36814dd2c1aafb22dadcb1e11e88539cf5b7e
Morel504 <ianmorelvalladares2003@gmail.com>
2023-02-03 12:58:37 -0600

    Tarea1 de Funciones
    Tarea 1

---
+++ Tareas/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.EjecutarButton = new System.Windows.Forms.Button();
+            this.PoNTextBox = new System.Windows.Forms.TextBox();
+            this.PoITextBox = new System.Windows.Forms.TextBox();
+            this.NumTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // EjecutarButton
+            // 
+            this.EjecutarButton.Location = new System.Drawing.Point(445, 163);
+            this.EjecutarButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.EjecutarButton.Name = "EjecutarButton";
+            this.EjecutarButton.Size = new System.Drawing.Size(292, 68);
+            this.EjecutarButton.TabIndex = 20;
+            this.EjecutarButton.Text = "Ejecutar";
+            this.EjecutarButton.UseVisualStyleBackColor = true;
+            this.EjecutarButton.Click += new System.EventHandler(this.EjecutarButton_Click);
+            // 
+            // PoNTextBox
+            // 
+            this.PoNTextBox.Location = new System.Drawing.Point(375, 345);
+            this.PoNTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.PoNTextBox.Name = "PoNTextBox";
+            this.PoNTextBox.Size = new System.Drawing.Size(416, 26);
+            this.PoNTextBox.TabIndex = 19;
+            // 
+            // PoITextBox
+            // 
+            this.PoITextBox.Location = new System.Drawing.Point(375, 277);
+            this.PoITextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.PoITextBox.Name = "PoITextBox";
+            this.PoITextBox.Size = new System.Drawing.Size(416, 26);
+            this.PoITextBox.TabIndex = 18;
+            // 
+            // NumTextBox
+            // 
+            this.NumTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.RecentlyUsedList;
+            this.NumTextBox.Location = new System.Drawing.Point(375, 85);
+            this.NumTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.NumTextBox.Name = "NumTextBox";
+            this.NumTextBox.Size = new System.Drawing.Size(416, 26);
+            this.NumTextBox.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(111, 351);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 20);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "¿Positivo o Negativo?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 277);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 20);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "¿Par o Impar?";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 85);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 20);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Ingrese su Número:";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(913, 441);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 15);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Ian Morel20211021997";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 465);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.EjecutarButton);
+            this.Controls.Add(this.PoNTextBox);
+            this.Controls.Add(this.PoITextBox);
+            this.Controls.Add(this.NumTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EjecutarButton;
+        private System.Windows.Forms.TextBox PoNTextBox;
+        private System.Windows.Forms.TextBox PoITextBox;
+        private System.Windows.Forms.TextBox NumTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Tareas/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Error
+        private void EjecutarButton_Click(object sender, EventArgs e)
+        {
+            if (NumTextBox.Text == String.Empty)
+            {
+                errorProvider1.SetError(NumTextBox, "Escriba un número");
+                return;
+            }
+            if (EjecutarButton.Text == String.Empty)
+            {
+                errorProvider1.SetError(NumTextBox, "Escriba un número");
+                return;
+            }
+            errorProvider1.Clear();
+
+            
+            int Num = Convert.ToInt32(NumTextBox.Text);
+            string R1, R2;
+
+            //Llamar las funciones
+            R1 = PoI(Num);
+            PoITextBox.Text = R1;
+
+            R2 = PoN(Num);
+            PoNTextBox.Text = R2;
+        }
+
+        //Funcion de: Par o impar
+        private string PoI(int num1)
+        {
+            string R;
+            int PoI = num1 % 2;
+            if (PoI == 0)
+            {
+                R = "Su número es par.";
+            }
+            else
+            {
+                R = "Su número es impar.";
+            }
+            return R;
+        }
+
+        //Funcion de: Positivo o Negativo
+        private string PoN(int num1)
+        {
+            string R;
+            int PoN = 0;
+
+            if (PoN >= 0) {
+                R = "Positivo";
+            }
+            else if (PoN < 0) { }
+            {
+                R = "Negativo";
+            }
+            return R;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
